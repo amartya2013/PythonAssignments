37606bc4ac6697761fc2c4e0ecfe756fe929ff63
amartya13 <amartya13.haldar@gmail.com>
2025-01-16 17:50:08 -0500

    This is my improved code for problem 4

--- problem4.py
+++ problem4.py
@@ -1,14 +1,15 @@
 def is_anagram(s,t):
     list_s = list(s)
+    clone_list_s = list_s
     list_t = list(t)
 
-    for i in list_t:
-        if i in list_s:
-            list_s.remove(i)
-        else:
-            print("they are not anagrams of each other")
-            break
-    if len(list_s)== 0:
+    for letter in list_t:
+        if letter in list_s:
+            clone_list_s.remove(letter)
+    if len(clone_list_s) == 0 and len(list(s)) == len(list(t)):
         print("they are anagrams")
+    else:
+        print("they are not anagrams")
 
-is_anagram('racecar', 'carrace')
+
+is_anagram(input("Enter first word here:"), input("Enter second word here:"))
