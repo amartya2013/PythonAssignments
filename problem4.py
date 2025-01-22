f4cfbb09b7e8c9706387cd35d959a1dea5875929
amartya13 <amartya13.haldar@gmail.com>
2025-01-22 14:20:13 -0500

    acciden

--- problem4.py
+++ problem4.py
@@ -1,8 +1,1 @@
-def is_anagram(s,t):
-    if len(s) == len(t) and set(s) == set(t):
-        print("they are anagrams")
-    else:
-        print("they are not anagrams")
 
-
-is_anagram(input("Enter first word here:"), input("Enter second word here:"))
