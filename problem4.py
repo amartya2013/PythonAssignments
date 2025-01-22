bd156ca9dd9e55261dddd29e676136dd1377128f
amartya13 <amartya13.haldar@gmail.com>
2025-01-22 14:33:20 -0500

    This is my optimized code for problem 4 after accidentally deleting

--- problem4.py
+++ problem4.py
@@ -1,1 +1,9 @@
 
+def is_anagram(s, t):
+    if set(s) == set(t) and len(s) == len(t):
+        print("they are anagram")
+    else:
+        print("they are not anagrams")
+
+
+is_anagram(input("first string:"), input("second string:"))
