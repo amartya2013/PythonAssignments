78ec37e708bff33ce62a6f6310f916a9bdac0ac3
amartya13 <amartya13.haldar@gmail.com>
2025-01-22 14:14:27 -0500

    optimized code and fixing accident

--- problem4.py
+++ problem4.py
@@ -1,11 +1,5 @@
 def is_anagram(s,t):
-    list_s = list(s)
-    clone_list_s = list_s
-    list_t = list(t)
-    for letter in list_t:
-        if letter in list_s:
-            clone_list_s.remove(letter)
-    if len(clone_list_s) == 0 and len(list(s)) == len(list(t)):
+    if len(s) == len(t) and set(s) == set(t):
         print("they are anagrams")
     else:
         print("they are not anagrams")
