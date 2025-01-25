8e66cc8cb9a1d63f8db40af068d33397ac2d5c2c
amartya13 <amartya13.haldar@gmail.com>
2025-01-25 10:12:21 -0500

    Optimizing code further

--- problem2.py
+++ problem2.py
@@ -1,12 +1,4 @@
 
-def take_input_and_return_list(string_separated_input: str) -> list:
-    return string_separated_input.split(',')
-
-
-def take_input_and_return_tuple(string_separated_input):
-    return tuple(string_separated_input.split(','))
-
-
 input_string = input("Enter comma separated string here:")
-print(take_input_and_return_list(input_string), take_input_and_return_tuple(input_string))
+print(list(input_string.split(',')), tuple(input_string.split(',')))
 
