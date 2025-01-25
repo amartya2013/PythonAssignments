4084ff86e1b23342317991da9d4433b92cf58cc1
amartya13 <amartya13.haldar@gmail.com>
2025-01-25 10:08:17 -0500

    Using the find() function to optimize the code

--- problem5.py
+++ problem5.py
@@ -1,10 +1,6 @@
 
 def find_target(target: int, target_list: list) -> int:
-    if target in target_list:
-        target_index = target_list.index(target)
-    else:
-        target_index = -1
-    return target_index
+    return ''.join(target_list).find(str(target))
 
 
 input_list = list(input("Enter a comma separated string:").split(','))
